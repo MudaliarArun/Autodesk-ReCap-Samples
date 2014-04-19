6a1d1a91683038cb1ab5640d1b7a893a55baf802
cyrillef <cyrillef@free.fr>
2014-04-19 21:24:34 +0200

    na

--- WpfCSharp/AutodeskWpfReCap/UserSettings.cs
+++ WpfCSharp/AutodeskWpfReCap/UserSettings.cs
@@ -1,4 +1,4 @@
-// (C) Copyright 2014 by Autodesk, Inc.
+﻿// (C) Copyright 2014 by Autodesk, Inc.
 //
 // The information contained herein is confidential, proprietary
 // to Autodesk, Inc., and considered a trade secret as defined
@@ -27,25 +27,25 @@ namespace Autodesk.ADN.WpfReCap {
 		// Hard coded consumer and secret keys and base URL.
 		// In real world Apps, these values need to secured.
 		// One approach is to encrypt and/or obfuscate these values
-		public static readonly string CONSUMER_KEY ="your consumer key" ;
-		public static readonly string CONSUMER_SECRET ="your consumer secret key" ;
-		public static readonly string O2_HOST ="https://accounts.autodesk.com/" ; // Autodesk production accounts server
+		public static readonly string CONSUMER_KEY = "your consumer key";
+		public static readonly string CONSUMER_SECRET = "your consumer secret key";
+		public static readonly string O2_HOST = "https://accounts.autodesk.com/"; // Autodesk production accounts server
 		//public static readonly string O2_HOST ="https://accounts-staging.autodesk.com/" ; // Autodesk staging accounts server
 
 		// ReCap: Fill in these macros with the correct information (only the 2 first are important)
-		public static readonly string ReCapAPIURL ="http://rc-api-adn.autodesk.com/3.1/API/" ;
-		public static readonly string ReCapClientID ="your ReCap client ID" ;
-		public static readonly string ReCapKey ="your ReCap client key" ; // not used anymore
-		public static readonly string ReCapUserID ="your ReCap user ID" ; // Needed only for using the ReCapSceneList, otherwise bail
+		public static readonly string ReCapAPIURL = "http://rc-api-adn.autodesk.com/3.1/API/";
+		public static readonly string ReCapClientID = "your ReCap client ID";
+		public static readonly string ReCapKey = "your ReCap client key"; // not used anymore
+		public static readonly string ReCapUserID = "your ReCap user ID"; // Needed only for using the ReCapSceneList, otherwise bail
 
-		public static readonly string Email ="your email address" ; // used for notification
+		public static readonly string Email = "your email address"; // used for notification
 
 		// Do not edit
-		public static readonly string O2_REQUESTTOKEN ="OAuth/RequestToken" ;
-		public static readonly string O2_ACCESSTOKEN ="OAuth/AccessToken" ;
-		public static readonly string O2_AUTHORIZE ="OAuth/Authorize" ;
-		public static readonly string O2_INVALIDATETOKEN ="OAuth/InvalidateToken" ;
-		public static readonly string O2_ALLOW =O2_HOST + "OAuth/Allow" ;
+		public static readonly string O2_REQUESTTOKEN = "OAuth/RequestToken";
+		public static readonly string O2_ACCESSTOKEN = "OAuth/AccessToken";
+		public static readonly string O2_AUTHORIZE = "OAuth/Authorize";
+		public static readonly string O2_INVALIDATETOKEN = "OAuth/InvalidateToken";
+		public static readonly string O2_ALLOW = O2_HOST + "OAuth/Allow";
 
 	}
 
