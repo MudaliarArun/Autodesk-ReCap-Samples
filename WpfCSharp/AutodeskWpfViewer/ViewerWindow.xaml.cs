0a186088d5d64b2ecce1066b0f379cf2d19affe8
cyrillef <cyrillef@free.fr>
2014-04-24 15:50:28 +0200

    Maya Viewport navigation vs Trackball

--- WpfCSharp/AutodeskWpfViewer/ViewerWindow.xaml.cs
+++ WpfCSharp/AutodeskWpfViewer/ViewerWindow.xaml.cs
@@ -47,17 +47,21 @@ namespace Autodesk.ADN.Toolkit.Wpf.Viewer {
 	enum DisplayMode { Wireframe, SmoothShade, WireframeOnShaded, Textured } ;
 
 	public partial class ViewerWindow : Window {
-		private Point _lastPos ;
 		private DisplayMode _currentDisplayMode =DisplayMode.Textured ;
 		private ModelVisual3D _currentMesh =null ;
 		private Material _currentMeshMatGroup =null ;
 		private ModelVisual3D _currentWireframe =null ;
 		private bool _wireframeDirty =true ;
-		private Vector3D _viewportT =new Vector3D () ;
-		private Quaternion _viewportR =new Quaternion () ;
+
+		AdskTrackball _trackball ;
+		AdskMayaOrbit _mayaOrbit ;
+		Adsk3dNavigate _currentNavigation ;
 		
 		public ViewerWindow () {
 			InitializeComponent () ;
+			_trackball =new AdskTrackball (viewport, camera) ;
+			_mayaOrbit =new AdskMayaOrbit (viewport, camera) ;
+			_currentNavigation =_mayaOrbit ;
 		}
 
 		public void LoadModel (string location) {
@@ -121,19 +125,7 @@ namespace Autodesk.ADN.Toolkit.Wpf.Viewer {
 		#region Controlling the 3D view camera
 		private void Grid_MouseDown (object sender, MouseButtonEventArgs e) {
 			Mouse.Capture (canvas, CaptureMode.Element) ;
-			_lastPos = Mouse.GetPosition (viewport) ; //e.GetPosition ()
-		}
-
-		private void Grid_MouseUp (object sender, MouseButtonEventArgs e) {
-			Mouse.Capture (canvas, CaptureMode.None) ;
-			if ( _wireframeDirty == true && (_currentDisplayMode == DisplayMode.Wireframe || _currentDisplayMode == DisplayMode.WireframeOnShaded ) ) {
-				model.Children.Remove (_currentWireframe) ;
-				BuildWireframeModel (0.0) ; //_currentDisplayMode == DisplayMode.Wireframe ? 0.0 : 1.0) ;
-				model.Children.Add (_currentWireframe) ;
-
-				foreach ( Visual3D child in model.Children )
-					CleanChildTransforms (child) ;
-			}
+			_currentNavigation.Position =Mouse.GetPosition (viewport) ; //e.GetPosition ()
 		}
 
 		private void Grid_MouseMove (object sender, MouseEventArgs e) {
@@ -142,25 +134,15 @@ namespace Autodesk.ADN.Toolkit.Wpf.Viewer {
 				Viewport_Rotate (pos) ;
 			else if ( e.MiddleButton == MouseButtonState.Pressed )
 				Viewport_Pan (pos) ;
-			_lastPos =pos ;
+		}
+
+		private void Grid_MouseUp (object sender, MouseButtonEventArgs e) {
+			Mouse.Capture (canvas, CaptureMode.None) ;
+			RebuildWireFrame () ;
 		}
 
 		private void Viewport_Pan (Point actualPos) {
-			Vector3D lastPos3D =ProjectToTrackball (_lastPos) ;
-			Vector3D pos3D =ProjectToTrackball (actualPos) ;
-
-			//Length(original_position - cam_position) / Length(offset_vector) = Length(zNearA - cam_position) / Length(zNearB - zNearA)
-			//offset_vector = Length(original_position - cam_position) / Length(zNearA - cam_position) * (zNearB - zNearA)
-			double halfFOV =(camera.FieldOfView / 2.0f) * (Math.PI / 180.0) ;
-			double distanceToObject =((Vector3D)camera.Position).Length ; // Compute the world space distance from the camera to the object you want to pan
-			double projectionToWorldScale =distanceToObject * Math.Tan (halfFOV) ;
-			Vector mouseDeltaInScreenSpace =actualPos - _lastPos ; // The delta mouse in pixels that we want to pan
-			Vector mouseDeltaInProjectionSpace =new Vector (mouseDeltaInScreenSpace.X * 2 / viewport.ActualWidth, mouseDeltaInScreenSpace.Y * 2 / viewport.ActualHeight) ; // ( the "*2" is because the projection space is from -1 to 1)
-			Vector cameraDelta =-mouseDeltaInProjectionSpace * projectionToWorldScale ; // Go from normalized device coordinate space to world space (at origin)
-
-			Vector3D tr =new Vector3D (0.0d, -cameraDelta.Y, -cameraDelta.X) ; // Remember we are up=<0,-1,0>
-			_viewportT +=tr ;
-
+			Vector3D tr =_currentNavigation.Viewport_Pan (actualPos) ;
 			foreach ( Visual3D child in model.Children ) {
 				Transform3DGroup transformGroup =child.Transform as Transform3DGroup ;
 				transformGroup.Children.Add (new TranslateTransform3D (tr)) ; // Remember we are up=<0,-1,0>
@@ -170,16 +152,7 @@ namespace Autodesk.ADN.Toolkit.Wpf.Viewer {
 		}
 
 		private void Viewport_Rotate (Point actualPos) {
-			Vector3D lastPos3D =ProjectToTrackball (_lastPos) ;
-			Vector3D pos3D =ProjectToTrackball (actualPos) ;
-			Vector3D axis =Vector3D.CrossProduct (lastPos3D, pos3D) ;
-			double angle =Vector3D.AngleBetween (lastPos3D, pos3D) ;
-
-			if ( axis.Length == 0 && angle == 0 )
-				return ;
-			Quaternion quat =new Quaternion (axis, -angle) ;
-			_viewportR =quat * _viewportR ;
-
+			Quaternion quat =_currentNavigation.Viewport_Rotate (actualPos) ;
 			QuaternionRotation3D r =new QuaternionRotation3D (quat) ;
 			foreach ( Visual3D child in model.Children ) {
 				Transform3DGroup transformGroup =child.Transform as Transform3DGroup ;
@@ -187,18 +160,6 @@ namespace Autodesk.ADN.Toolkit.Wpf.Viewer {
 			}
 
 			_wireframeDirty =true ;
-		}
-
-		// http://scv.bu.edu/documentation/presentations/visualizationworkshop08/materials/opengl/trackball.c
-		// http://curis.ku.dk/ws/files/38552161/01260772.pdf
-		private Vector3D ProjectToTrackball (Point pos) { // Project an <x, y> pair onto a sphere
-			// Translate 0,0 to the center, so <x, y> is [<-1, -1> - <1, 1>]
-			double x =pos.X / (viewport.ActualWidth / 2) - 1 ;
-			double y =1 - pos.Y / (viewport.ActualHeight / 2) ; // Flip Y - up instead of down
-			double z2 =1 - Math.Pow (x, 2) - Math.Pow (y, 2) ; // z^2 =1 - x^2 - y^2
-			double z =(z2 > 0 ? Math.Sqrt (z2) : 0) ;
-			// Remember we are up=<0,-1,0>
-			return (new Vector3D (-z, -y, x)) ;
 		}
 
 		private void Grid_MouseWheel (object sender, MouseWheelEventArgs e) {
@@ -212,41 +173,46 @@ namespace Autodesk.ADN.Toolkit.Wpf.Viewer {
 			transformGroup.Children.Add (new TranslateTransform3D (lookAt)) ;
 
 			_wireframeDirty =true ;
-			if ( _currentDisplayMode == DisplayMode.Wireframe || _currentDisplayMode == DisplayMode.WireframeOnShaded ) {
-				model.Children.Remove (_currentWireframe) ;
-				BuildWireframeModel (0.0) ; //_currentDisplayMode == DisplayMode.Wireframe ? 0.0 : 1.0) ;
-				model.Children.Add (_currentWireframe) ;
+			RebuildWireFrame () ;
+		}
 
-				foreach ( Visual3D child in model.Children )
-					CleanChildTransforms (child) ;
-			}
+		private void RebuildWireFrame () {
+			if ( _wireframeDirty == false || (_currentDisplayMode != DisplayMode.Wireframe && _currentDisplayMode != DisplayMode.WireframeOnShaded ) )
+				return ;
+			model.Children.Remove (_currentWireframe) ;
+			BuildWireframeModel (0.0) ; //_currentDisplayMode == DisplayMode.Wireframe ? 0.0 : 1.0) ;
+			model.Children.Add (_currentWireframe) ;
+
+			foreach ( Visual3D child in model.Children )
+				CleanChildTransforms (child) ;
 		}
 
 		private void CleanChildTransforms (Visual3D child) {
 			Transform3DGroup transformGroup =child.Transform as Transform3DGroup ;
 			for ( int i =(child == _currentMesh || child == _currentWireframe ? 2 : 0) ; i < transformGroup.Children.Count ; /*i++*/ )
 				transformGroup.Children.RemoveAt (i) ;
-			if ( _viewportT.Length != 0 )
-				transformGroup.Children.Add (new TranslateTransform3D (_viewportT)) ;
-			if ( !_viewportR.IsIdentity )
-				transformGroup.Children.Add (new RotateTransform3D (new QuaternionRotation3D (_viewportR))) ;
+			if ( _currentNavigation.Translation.Length != 0 )
+				transformGroup.Children.Add (new TranslateTransform3D (_currentNavigation.Translation)) ;
+			if ( !_currentNavigation.Rotation.IsIdentity )
+				transformGroup.Children.Add (new RotateTransform3D (new QuaternionRotation3D (_currentNavigation.Rotation))) ;
 		}
 
+		#endregion
+
+		#region Viewport Options
 		private void Home_Click (object sender, RoutedEventArgs e) {
 			if ( e != null )
 				e.Handled =true ;
 
-			_viewportT =new Vector3D () ;
-			_viewportR =new Quaternion () ;
+			//_viewportT =new Vector3D () ;
+			//_viewportR =new Quaternion () ;
+			_trackball.Clear () ;
+			_mayaOrbit.Clear () ;
 
 			// 0 and 1 are origin scale and translate
-			foreach ( Visual3D child in model.Children ) {
-				//Transform3DGroup transformGroup =child.Transform as Transform3DGroup ;
-				//for ( int i =(child == _currentMesh ? 2 : 0) ; i < transformGroup.Children.Count ; /*i++*/ )
-				//	transformGroup.Children.RemoveAt (i) ;
+			foreach ( Visual3D child in model.Children )
 				CleanChildTransforms (child) ;
-			}
-
+	
 			Transform3DGroup transformGroup =camera.Transform as Transform3DGroup ;
 			transformGroup.Children.Clear () ;
 		}
@@ -312,6 +278,16 @@ namespace Autodesk.ADN.Toolkit.Wpf.Viewer {
 
 			foreach ( Visual3D child in model.Children )
 				CleanChildTransforms (child) ;
+		}
+
+		private void trackballToggle_Checked (object sender, RoutedEventArgs e) {
+			e.Handled =true ;
+			_currentNavigation =_trackball ;
+		}
+
+		private void trackballToggle_Unchecked (object sender, RoutedEventArgs e) {
+			e.Handled =true ;
+			_currentNavigation =_mayaOrbit ;
 		}
 
 		private void ambiantlightToggle_Checked (object sender, RoutedEventArgs e) {
